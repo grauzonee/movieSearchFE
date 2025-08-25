cb5e10c0ae07a142cce2d2af69b944a0a3c01a88
Dumm Devka <dummdevka@Dumms-MacBook-Air.local>
2025-08-25 20:30:51 +0200

    Improve create form design + add transition

--- app/src/components/RouterButton.vue
+++ app/src/components/RouterButton.vue
@@ -13,7 +13,7 @@ defineProps({
 <template>
   <RouterLink :to="to">
     <button
-      class="border border-emerald-400 rounded-md px-2 pointer hover:bg-white hover:text-gray-800 hover:shadow-md"
+      class="border border-emerald-400 rounded-md px-2 pointer hover:bg-white hover:text-gray-800 hover:shadow-md transition-all duration-300"
     >
       {{ text }}
     </button>
--- app/src/components/TagsForm.vue
+++ app/src/components/TagsForm.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import BaseInput from '@/components/BaseInput.vue'
 import { ref } from 'vue'
 
 const currentTag = ref('')
@@ -8,22 +9,34 @@ defineProps<{
     type: string[]
   }
 }>()
-const emit = defineEmits(['updateTags'])
+const emit = defineEmits(['updateTags', 'deleteTag'])
 
 function onInput() {
   emit('updateTags', currentTag.value)
   currentTag.value = ''
 }
+
+function onDelete(tag) {
+  emit('deleteTag', tag)
+}
 </script>
 <template>
   <div class="flex flex-col w-full gap-2">
-    <input
+    <BaseInput
       placeholder="Type..."
-      class="rounded-sm px-3 shadow-md outline-none mb-3"
       @keydown.space.prevent="onInput"
       @keydown.enter.prevent="onInput"
       v-model="currentTag"
     />
-    <div v-for="(tag, index) in tags" :key="index" class="rounded-sm px-1">{{ tag }}</div>
+    <div class="flex flex-row flex-wrap gap-2 w-full">
+      <div
+        v-for="(tag, index) in tags"
+        :key="index"
+        class="rounded-lg px-3 font-bold px-1 bg-emerald-300/80 cursor-pointer"
+        @click="onDelete(tag)"
+      >
+        {{ tag }}
+      </div>
+    </div>
   </div>
 </template>
