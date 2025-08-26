865ec4004867a1621decf257093c1fe8a1dfe834
Dumm Devka <dummdevka@Dumms-MacBook-Air.local>
2025-08-26 11:08:07 +0200

    Change movieBlock look + remove border around inputs

--- app/src/components/BaseInput.vue
+++ app/src/components/BaseInput.vue
@@ -6,13 +6,13 @@ const model = defineModel<string>()
   <textarea
     v-if="textarea"
     v-model="model"
-    class="border border-white/20 bg-white/80 rounded-lg focus:ring-2 focus:ring-green-400 focus:outline-none py-3 px-2 focus:h-32"
+    class="border border-white/20 bg-white/80 rounded-lg focus:shadow-lg focus:outline-none py-3 px-2 focus:h-32 transition-all duration-500"
     :placeholder="placeholder"
   ></textarea>
   <input
     v-else
     v-model="model"
-    class="border border-white/20 bg-white/80 rounded-lg focus:ring-2 focus:ring-green-400 focus:outline-none py-3 px-2"
+    class="border border-white/20 bg-white/60 rounded-lg focus:shadow-lg outline-none py-3 px-2 transition-all duration-500"
     :placeholder="placeholder"
   />
 </template>
--- app/src/components/MovieBlock.vue
+++ app/src/components/MovieBlock.vue
@@ -7,8 +7,17 @@ defineProps({
 })
 </script>
 <template>
-  <div class="rounded-md bg-white text-gray-700 p-4 shadow w-full" v-if="movie">
-    <span class="font-bold mb-2 block">{{ movie.title }}</span>
-    <span class="text-xs mb-3 block">{{ movie.plot }}</span>
+  <div class="w-full p-4 rounded-2xl shadow-md hover:shadow-lg transition" v-if="movie">
+    <span class="font-bold mb-2 block text-xl">{{ movie.title }}</span>
+    <span class="text-xs mb-3 block line-clamp-4">{{ movie.plot }}</span>
+    <div class="flex flex-wrap gap-2 mt-2">
+      <div
+        class="bg-green-100/40 text-gray-600 text-xs py-1 font-semibold px-2 rounded-md shadow-md"
+        v-for="(genre, key) in movie.genres"
+        :key="key"
+      >
+        {{ genre }}
+      </div>
+    </div>
   </div>
 </template>
